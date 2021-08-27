35a2c88b76566156e350241b3a3120bc89cac137
kubersharma001 <kubersharma001@gmail.com>
2021-08-27 17:25:10 +0000

    fogos: Import powerhint.json from coral
    * From: https://android.googlesource.com/device/google/coral/+/android-11.0.0_r42/powerhint.json
    
    Change-Id: I8dba2912afb27e0432bdce601ce38f2934bb0186

--- device.mk
+++ device.mk
@@ -83,6 +83,9 @@ $(foreach DEVICE_SKU, $(DEVICE_SKUS), \
 # Power
 $(call soong_config_set,qtipower,tap_to_wake_node,/sys/class/touchscreen/primary/gesture)
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # Properties
 PRODUCT_PACKAGES += \
     hardware.sku.XT2363-8.prop \
